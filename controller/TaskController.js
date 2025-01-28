f7d85e47160d7aaf1f0f4fd509f412f71bedc981
Fernando Cavalcante <odntht@gmail.com>
2025-01-28 09:02:53 -0300

    aula 04 - add getByID, update e delete task by ID e racunho de modal de confirmação ao solicitar apagar

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -3,18 +3,16 @@ const Task = require('../models/Task')
 const getAllTasks = async (req, res) => {
     try {
         const taskLists = await Task.find()
-        return res.render("index", {taskLists});
+        return res.render("index", { taskLists, task: null, taskDelete: null });
     }
     catch (err) {
-        // console.log(err)
         res.status(500).send({ error: err.msg })
     }
-   
 }
 
 const createTask = async (req, res) => {
     const task = req.body
-    if(task.check === undefined){
+    if (task.check === undefined) {
         task.check = false
     }
     if (!task.task) {
@@ -30,7 +28,45 @@ const createTask = async (req, res) => {
 
 }
 
+const getById = async (req, res) => {
+    try {
+        const taskLists = await Task.find()
+        if (req.params.method == 'update') {
+            const task = await Task.findOne({ _id: req.params.id })
+            return res.render('index', { taskLists, task, taskDelete: null })
+        } else {
+            const taskDelete = await Task.findOne({ _id: req.params.id })
+            return res.render('index', { taskLists, task: null, taskDelete })
+        }
+    } catch (err) {
+        res.status(500).send({ error: err.msg })
+    }
+
+}
+
+const updateOneTask = async (req, res) => {
+    try {
+        const task = req.body
+        await Task.updateOne({ _id: req.params.id }, task)
+        return res.redirect('/')
+    } catch (err) {
+        res.status(500).send({ error: err.msg })
+    }
+}
+
+const deleteOneTask = async (req, res) => {
+    try {
+        await Task.deleteOne({ _id: req.params.id })
+        return res.redirect('/')
+    } catch (err) {
+        res.status(500).send({ error: err.msg })
+    }
+}
+
 module.exports = {
     getAllTasks,
-    createTask
+    createTask,
+    getById,
+    updateOneTask,
+    deleteOneTask
 }
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = 3000
 
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.urlencoded())
+app.use(express.urlencoded({extended: true}))
 app.use(routes)
 
 app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`)
